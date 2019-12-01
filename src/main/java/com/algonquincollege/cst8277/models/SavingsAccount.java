0e73c132ebe944c5463e0b8a646645c96462c81f
Vi Pham <vipham25896@gmail.com>
2019-12-01 03:20:42 -0500

    10 test cases

--- src/main/java/com/algonquincollege/cst8277/models/SavingsAccount.java
+++ src/main/java/com/algonquincollege/cst8277/models/SavingsAccount.java
@@ -13,7 +13,6 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-
 /**
  * The persistent class for the ACCOUNT database table.
  * 
@@ -21,16 +20,21 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue(value = "S")
 public class SavingsAccount extends AccountBase implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private double savingRate;
+    private static final long serialVersionUID = 1L;
 
-	@Column(name = "RATE")
+    public SavingsAccount() {
+
+    }
+
+    private double savingRate;
+
+    @Column(name = "RATE")
     public double getSavingRate() {
         return savingRate;
     }
+
     public void setSavingRate(double savingRate) {
         this.savingRate = savingRate;
     }
-	
+
 }
